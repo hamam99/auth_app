e2cd5821fc14ff635fe15f038462f1a395408add
hamam <hamam.nasrodin@appfuxion.com>
2026-01-30 17:09:00 +0800

    feat: add splash screen with app icon and title
    Introduce a splash screen component that displays the app icon and title for 800ms before navigating to the appropriate authentication flow. This improves the initial user experience by providing a branded loading state.

--- src/navigation/RootNavigation.tsx
+++ src/navigation/RootNavigation.tsx
@@ -2,11 +2,24 @@ import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigation from './AuthNavigation';
 import NonAuthNavigation from './NonAuthNavigation';
 import { useAuth } from '../context/AuthContext';
+import { useEffect, useState } from 'react';
+import Splash from '../pages/splash';
 
 const RootNavigation = () => {
   const { isAlreadyLogin } = useAuth();
+  const [isShowSplashScreen, setIsShowSplashScreen] = useState(true);
 
-  console.log(`isAlreadyLogin`, { isAlreadyLogin });
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsShowSplashScreen(false);
+    }, 800);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isShowSplashScreen) {
+    return <Splash />;
+  }
+
   return (
     <NavigationContainer>
       {isAlreadyLogin ? <AuthNavigation /> : <NonAuthNavigation />}
---
+++ src/pages/splash/index.tsx
@@ -0,0 +1,32 @@
+import { Image, StyleSheet, Text, View } from 'react-native';
+import Images from '../../assets/images';
+import COLORS from '../../contants/Colors';
+
+const Splash = () => {
+  return (
+    <View style={styles.container}>
+      <Image source={Images.app_icon} style={styles.logo} />
+
+      <Text style={styles.title}>Simple Auth App</Text>
+    </View>
+  );
+};
+
+export default Splash;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 10,
+  },
+  logo: {
+    width: 120,
+    height: 120,
+  },
+  title: {
+    fontSize: 22,
+    color: COLORS.blue_1,
+  },
+});
